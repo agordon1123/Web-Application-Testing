248e17adb6e84a999e8091428bc2230ee386f412
Alex <agordon1123@gmail.com>
2019-08-08 16:44:31 -0400

    app laid out

--- web-application-testing/src/App.js
+++ web-application-testing/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import Dashboard from './components/Dashboard';
 import Display from './components/Display';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <Dashboard />
       <Display />
     </div>
   );
--- web-application-testing/src/components/Display.js
+++ web-application-testing/src/components/Display.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Dashboard from './Dashboard';
 
 const Display = () => {
+    const [homeScore, setHomeScore] = useState(0);
+    const [awayScore, setAwayScore] = useState(0);
     return (
         <div>
-            <h1>Hello World</h1>
+            <div className='teams'>
+                <img src="https://img.icons8.com/dotty/80/000000/dragon.png" />
+                    <p>at</p>
+                <img src="https://img.icons8.com/dotty/80/000000/tiger.png" />
+            </div>
+
+            <Dashboard />
         </div>
     )
 }
