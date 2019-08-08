1e9675da86ae8231d0aa510c6c05c526811424b0
Alex <agordon1123@gmail.com>
2019-08-08 15:13:56 -0400

    react-app initialized

---
+++ web-application-testing/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Dashboard from './components/Dashboard';
+import Display from './components/Display';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Dashboard />
+      <Display />
+    </div>
+  );
+}
+
+export default App;
